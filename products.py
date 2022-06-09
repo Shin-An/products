9754f388d6b019163964e236644beb600d4a6812
Shin-An <siancat0525@gmail.com>
2022-06-09 17:29:42 +0800

    add read file

--- products.py
+++ products.py
@@ -1,23 +1,28 @@
-# 二維度清單
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line: # 跳過商品及價格，continue為跳到下一迴，跟break一樣只能寫在迴圈裡
+            continue # 跳過7-8行迴圈內容不執行
+        name, price = line.strip().split(',') # split為切割，()內為切割的標準，切割完的結果是清單
+        products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q': # quit
         break
     price = input('請輸入商品價格: ')
-    # p = []
-    # p.append(name)
-    # p.append(price)
-    # p = [name, price]
-    # products.append(p)
+    price = int(price)
     products.append([name, price])
 print(products)
-# products[0][0] # 意即products中的第0個大清單中，第0個小清單
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
-# csv為電腦常用儲存資料的檔案格式，可用excel開啟，用','分隔可將資料分成excel中的兩格
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
